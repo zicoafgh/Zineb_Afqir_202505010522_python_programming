f8c7f231009e387d736db362b46642ddcf466603
zicoafgh <35202930+zicoafgh@users.noreply.github.com>
2026-06-24 08:13:51 +0000

    StudentAverage Calculation

--- week3/Student_Av_Calc.py
+++ week3/Student_Av_Calc.py
@@ -1,1 +1,25 @@
 
+choice = "Y"
+
+# Use loop condition to keep the user choice open 
+while choice == "Y":
+
+    # Ask the user to enter three marks of his quizzes
+    quiz_1 = float(input("Enter Quiz 1 mark: "))
+    quiz_2 = float(input("Enter Quiz 2 mark: "))
+    quiz_3 = float(input("Enter Quiz 3 mark: "))
+
+    # Average Calculation
+    
+    average = (quiz_1 + quiz_2 + quiz_3)/ 3
+
+    # Check if the student passes or fails
+    if average >= 50:
+        print("Congratulations, You've passed")
+    else:
+        print("Unfortunately, you've failed.")
+
+    # Ask the user if he wants to continue or not
+    choice = input("Continue? Select Y/N: ").upper()
+
+print("Program Ended")
